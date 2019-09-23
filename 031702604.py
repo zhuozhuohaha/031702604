be4de2ba8a878bb63e2cc1560bcd58bb9137bd75
zhuozhuohaha <54981107+zhuozhuohaha@users.noreply.github.com>
2019-09-23 20:22:49 +0800

    Update 031702604.py

--- 031702604.py
+++ 031702604.py
@@ -182,7 +182,6 @@ if x[0]=="2":
                     m=re.sub(p,"",m,1)
                 t.append(b)
                 break
-    print(m)
     k=re.match(r'.*?(县|区|市)',m)
     if k:
         c=k.group()
